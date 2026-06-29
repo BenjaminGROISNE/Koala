b5e9fe19cd6602283971c512b4e1f21c2816c129
Benjamin GROISNE <125885157+BenjaminGROISNE@users.noreply.github.com>
2026-06-29 19:44:59 +0200

    Publish Koala 1.0.0 from BotMaker Studio

---
+++ src/main/java/com/koala/Koala.java
@@ -0,0 +1,8 @@
+package com.koala;
+import com.botmaker.sdk.api.BotMaker;
+
+public class Koala {
+    public static void main(String[] args) {
+        BotMaker.print("Hello from Koala!");
+    }
+}
